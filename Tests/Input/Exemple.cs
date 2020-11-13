73250992a68f518edf42beb8e8fc270bd708152f
polina725 <62598798+polina725@users.noreply.github.com>
2020-11-13 14:43:22 +0300

    added unit tests

---
+++ Tests/Input/Exemple.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exmpl
+{
+    class Exemple
+    {
+        public void FirstMethod()
+        {
+
+        }
+
+        private void SecondMethod()
+        {
+
+        }
+
+        private string ThirdMethod()
+        {
+            return null;
+        }
+    }
+
+    class AnotherClass
+    {
+        public void FirstMethodA()
+        {
+
+        }
+
+        public void SecondMethodA()
+        {
+
+        }
+
+        private string ThirdMethodA()
+        {
+            return null;
+        }
+    }
+
+    class EmptyClass
+    {
+
+    }
+}
